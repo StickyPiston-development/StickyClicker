4f849b897d235d1b2070fbec9ca226ba7f32b8ea
Sticky Piston <mr.sticky.piston@gmail.com>
2023-01-25 11:26:28 +0100

    Removed unused files

--- StickyClicker/test/sub.py
+++
@@ -1,5 +0,0 @@
-import subprocess, time
-
-process = subprocess.Popen("python StickyClicker/stickyclicker.py", shell=True)
-time.sleep(5)
-subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=process.pid))
